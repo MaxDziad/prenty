5721a7ddce2adaae4832c15bf3e6b2e612a55887
MaxPortablePC <dziadon.kakuzu@gmail.com>
2024-11-30 13:53:37 +0100

    fixing mouse controller

--- prenty/Assets/Scripts/Player/MouseCursorController.cs
+++ prenty/Assets/Scripts/Player/MouseCursorController.cs
@@ -5,6 +5,14 @@ public class MouseCursorController : MonoBehaviour, ISceneObject
 	[SerializeField]
 	private float _cursorSpeed;
 
+	[SerializeField]
+	[Range(0f, 1f)]
+	private float _xRangeThreshold = 0;
+
+	[SerializeField]
+	[Range(0f, 1f)]
+	private float _yRangeThreshold = 0;
+
 	private GameplayInputProviderSystem _gameplayInputProviderSystem;
 
 	public void OnInitialize()
@@ -19,7 +27,18 @@ public class MouseCursorController : MonoBehaviour, ISceneObject
 
 	private void OnLook(Vector2 vector)
 	{
-		transform.position += new Vector3(vector.x, vector.y, 0) * Time.deltaTime * _cursorSpeed;
+		var newTargetPosition = transform.position;
+		newTargetPosition += new Vector3(vector.x, vector.y, 0) * Time.deltaTime * _cursorSpeed;
+		var screenPoint = Camera.main.WorldToViewportPoint(newTargetPosition);
+
+		if (screenPoint.x >= 0 + _xRangeThreshold && screenPoint.x <= 1 - _xRangeThreshold)
+		{
+			transform.position = new Vector2(newTargetPosition.x, transform.position.y);
+		}
+		if (screenPoint.y >= 0 + _yRangeThreshold && screenPoint.y <= 1 - _yRangeThreshold)
+		{
+			transform.position = new Vector2(transform.position.x, newTargetPosition.y);
+		}
 	}
 
 	private void OnDestroy()
