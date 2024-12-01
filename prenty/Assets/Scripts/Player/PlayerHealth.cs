623586a870fec458a40ddcb6819154133e716925
shroobi <dsquer888@gmail.com>
2024-12-01 02:44:18 +0100

    Added collision and taking damage

--- prenty/Assets/Scripts/Player/PlayerHealth.cs
+++ prenty/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Rendering.Universal;
 
 public class PlayerHealth : MonoBehaviour
@@ -6,12 +7,22 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private Light2D _flashlightCone;
     [SerializeField] private Light2D _flashlightInner;
     [SerializeField] private int _fullHealth = 3;
+
+    private float _flashlightConeBaseO;
+    private float _flashlightInnerBaseO;
+    private float _flashlightConeBaseI;
+    private float _flashlightInnerBaseI;
     
     private int _currentHealth;
+    public UnityEvent OnGameOver;
     
     void Start()
     {
         _currentHealth = _fullHealth;
+        _flashlightConeBaseO = _flashlightCone.pointLightOuterRadius;
+        _flashlightInnerBaseO = _flashlightInner.pointLightOuterRadius;
+        _flashlightConeBaseI = _flashlightCone.pointLightInnerRadius;
+        _flashlightInnerBaseI = _flashlightInner.pointLightInnerRadius;
     }
 
     void TakeDamage()
@@ -21,12 +32,28 @@ public class PlayerHealth : MonoBehaviour
             _currentHealth--;
             ChangeIntensity((float)_currentHealth/(float)_fullHealth);
         }
-        else Debug.Log("You ded bruh");
+        else
+        {
+            OnGameOver?.Invoke();
+        }
     }
 
     void ChangeIntensity(float newValue)
     {
-        _flashlightCone.intensity = newValue;
-        _flashlightInner.intensity = newValue;
+        _flashlightCone.pointLightOuterRadius = newValue * _flashlightConeBaseO;
+        _flashlightInner.pointLightOuterRadius = newValue * _flashlightInnerBaseO;
+        _flashlightCone.pointLightInnerRadius = newValue * _flashlightConeBaseI;
+        _flashlightInner.pointLightInnerRadius = newValue * _flashlightInnerBaseI;
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log(collision.tag);
+        if (collision.CompareTag("Enemy"))
+        {
+            Debug.Log("ała kurwa");
+            TakeDamage();
+        }
     }
 }
